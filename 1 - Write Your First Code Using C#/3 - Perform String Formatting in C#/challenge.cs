5ca149be01fea6e6b4db59a859205914fc5cecb1
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-10-16 20:53:12 -0300

    First challenges of the first module

---
+++ 1 - Write Your First Code Using C#/3 - Perform String Formatting in C#/challenge.cs
@@ -0,0 +1,14 @@
+string projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+string projectBasePath = $@"c:\Exercise\{projectName}";
+
+string projectFileName = "data.txt";
+
+Console.WriteLine($@"View English output:
+    {projectBasePath}\{projectFileName}
+");
+
+Console.WriteLine($@"{russianMessage}:
+    {projectBasePath}\ru-RU\{projectFileName}");
---
+++ 1 - Write Your First Code Using C#/4 - Perform Basic Operations on Numbers in C#/challenge.cs
@@ -0,0 +1,5 @@
+int fahrenheit = 94;
+
+decimal celsiusTemperature = (fahrenheit - 32) * 5.0m/9;
+
+Console.WriteLine($"The temperature is {celsiusTemperature} Celsius.");
