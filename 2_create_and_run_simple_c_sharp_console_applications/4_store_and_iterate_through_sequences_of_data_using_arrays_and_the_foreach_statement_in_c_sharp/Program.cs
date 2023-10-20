db38500a09ace606754a7bd8fd7bf3e5b9624ddb
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-10-19 22:07:07 -0300

    Two new modules from the second course

---
+++ 2_create_and_run_simple_c_sharp_console_applications/4_store_and_iterate_through_sequences_of_data_using_arrays_and_the_foreach_statement_in_c_sharp/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] matrix = {
+    "B123",
+    "C234",
+    "A345",
+    "C15",
+    "B177",
+    "G3003",
+    "C235",
+    "B179"
+};
+
+foreach(string product in matrix) {
+    if (product.StartsWith("B")) {
+        Console.WriteLine(product);
+    }
+}
---
+++ 2_create_and_run_simple_c_sharp_console_applications/5_create_readable_code_with_conventions_whitespace_and_comments_in_c_sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿string message = "The quick brown fox jumps over the lazy dog.";
+
+char[] charArrayMessage = message.ToCharArray();
+Array.Reverse(charArrayMessage);
+
+int numOs = 0;
+
+// count the o's
+foreach (char i in charArrayMessage) 
+{ 
+    if (i == 'o') 
+    { 
+        numOs++; 
+    } 
+}
+
+string reversedMessage = new String(charArrayMessage);
+
+Console.WriteLine(reversedMessage);
+Console.WriteLine($"'o' appears {numOs} times.");
