07bfa1706b533166d3951fc3836febe041a06c75
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-10-24 15:24:16 -0300

    First four modules of the third course

---
+++ 3_add_logic_to_c_sharp_console_applications/1_evaluate_boolean_expressions_to_make_decisions_in_c_sharp/Program.cs
@@ -0,0 +1,19 @@
+﻿string permission = "Admin|Manager";
+int level = 55;
+
+string message;
+if (permission.Contains("Admin"))
+{
+    string positionName = level > 55 ? "Super Admin" : "Admin";
+    message = $"Welcome, {positionName} user";
+}
+else if (permission.Contains("Manager") && level >= 20)
+{
+    message = "Contact an Admin for access.";
+}
+else
+{
+    message = "You do not have sufficient privileges.";
+}
+
+Console.WriteLine(message);
