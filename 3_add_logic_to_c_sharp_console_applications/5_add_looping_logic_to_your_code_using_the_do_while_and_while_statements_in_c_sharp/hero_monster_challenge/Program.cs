eec3cd380d4dccb10904053efa5988e6e971736b
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-10-24 22:00:11 -0300

    First challenge of the 5th module of the 3rd course

---
+++ 3_add_logic_to_c_sharp_console_applications/5_add_looping_logic_to_your_code_using_the_do_while_and_while_statements_in_c_sharp/hero_monster_challenge/Program.cs
@@ -0,0 +1,22 @@
+﻿int heroIntegrity = 10;
+int monsterIntegrity = 10;
+
+do
+{
+    Random attacks = new Random();
+
+    int heroAttack = attacks.Next(11);
+    monsterIntegrity -= heroAttack;
+
+    Console.WriteLine($"Monster was damaged and lost {heroAttack} health and now has {monsterIntegrity} health.");
+    
+    if (monsterIntegrity <= 0)
+        continue;
+
+    int monsterAttack = attacks.Next(11);
+    heroIntegrity -= monsterAttack;
+
+    Console.WriteLine($"Hero was damaged and lost {monsterAttack} health and now has {heroIntegrity} health.");
+} while (heroIntegrity > 0 && monsterIntegrity > 0);
+
+Console.WriteLine(heroIntegrity > monsterIntegrity? "Hero wins!" : "Monster wins!");
