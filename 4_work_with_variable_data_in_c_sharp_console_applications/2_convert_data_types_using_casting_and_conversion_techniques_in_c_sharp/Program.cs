6089522c9df0e9c6c735334aa4c9d2c37172a021
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-10-30 21:34:55 -0300

    First three modules of the 4th course

---
+++ 4_work_with_variable_data_in_c_sharp_console_applications/2_convert_data_types_using_casting_and_conversion_techniques_in_c_sharp/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+
+string message = "";
+decimal total = 0;
+
+bool parseResult;
+
+foreach(string value in values)
+{
+    decimal valueAsDecimal;
+
+    parseResult = decimal.TryParse(value, out valueAsDecimal);
+
+    if (parseResult)
+        total += valueAsDecimal;
+    else
+        message += value;
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}\n");
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+int result1 = Convert.ToInt32(value1/value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2/(decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3/value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
---
+++ 4_work_with_variable_data_in_c_sharp_console_applications/3_perform_operations_on_arrays_using_helper_methods_in_c_sharp/Program.cs
@@ -0,0 +1,29 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+
+string[] pangramSplitted = pangram.Split(' ');
+
+for(int i = 0; i < pangramSplitted.Length; i++)
+{
+    char[] wordCharArray = pangramSplitted[i].ToCharArray();
+    Array.Reverse(wordCharArray);
+    pangramSplitted[i] = new string(wordCharArray);
+}
+
+string result = String.Join(' ', pangramSplitted);
+Console.WriteLine(result);
+
+Console.WriteLine();
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderIds = orderStream.Split(',');
+
+Array.Sort(orderIds);
+
+for(int i = 0; i < orderIds.Length; i++)
+{
+    if (orderIds[i].Length != 4)
+        orderIds[i] += "\t- Error";
+
+    Console.WriteLine(orderIds[i]);
+}
