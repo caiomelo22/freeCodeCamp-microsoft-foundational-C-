324ab4cab0b6d7fa95ee5a3c375b8fd8d8c78e6a
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-11-03 15:13:28 -0300

    Second module of the 5th course

---
+++ 5_create_methods_in_c_sharp_console_applications/2_create_c_sharp_methods_with_parameters/Program.cs
@@ -0,0 +1,29 @@
+﻿void PrintEmailAddres(string name, string lastName, string domain = "contoso.com")
+{
+    Console.WriteLine($"{name.Substring(0,2)}{lastName}@{domain}".ToLower());
+}
+
+string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    PrintEmailAddres(name: corporate[i, 0], lastName: corporate[i, 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    PrintEmailAddres(name: external[i, 0], lastName: external[i, 1], domain: externalDomain);
+}
