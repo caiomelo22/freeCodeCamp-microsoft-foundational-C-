5eca6d1e9b6684049053c0ccd0e3ee1124e24559
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-11-05 17:31:13 -0300

    Third module of the 5th course

---
+++ 5_create_methods_in_c_sharp_console_applications/3_create_c_sharp_methods_that_return_values/Program.cs
@@ -0,0 +1,56 @@
+﻿Random random = new Random();
+
+int generateRandomInt(int startRange, int endRange)
+{
+    return random.Next(startRange, endRange + 1);
+}
+
+bool ShouldPlay()
+{
+    string? readResult;
+    string decision = "";
+
+    do
+    {
+        readResult = Console.ReadLine();
+        if (readResult != null)
+        {
+            decision = readResult.ToLower();
+        }
+
+        if (decision == "y" || decision == "n")
+            return decision == "y";
+    } while (true);
+}
+
+string WinOrLose(int roll, int target)
+{
+    if (roll > target)
+        return "You win!";
+    else
+        return "You lose!";
+}
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay())
+{
+    PlayGame();
+}
+
+void PlayGame()
+{
+    var play = true;
+
+    while (play)
+    {
+        var target = generateRandomInt(1, 5);
+        var roll = generateRandomInt(1, 6);
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose(roll, target));
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
+    }
+}
