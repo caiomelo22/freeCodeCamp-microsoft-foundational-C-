ae72442019cc527919b974f9a9e72b8f3ae09bce
Caio Graça Melo <caio.g.melo@hotmail.com>
2023-11-06 11:56:42 -0300

    Guided and challenge projects of the 5th course

---
+++ 5_create_methods_in_c_sharp_console_applications/4_guided_project_plan_a_petting_zoo_visit/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+string[,] group;
+
+RandomizeAnimals();
+group = AssignGroup();
+Console.WriteLine("School A");
+PrintGroup(group);
+
+RandomizeAnimals();
+group = AssignGroup(3);
+Console.WriteLine("School B");
+PrintGroup(group);
+
+RandomizeAnimals();
+group = AssignGroup(2);
+Console.WriteLine("School C");
+PrintGroup(group);
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++) 
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[i];
+        pettingZoo[i] = pettingZoo[r];
+        pettingZoo[r] = temp;
+    }
+}
+
+void PrintGroup(string[,] groups)
+{
+    int numGroups = groups.GetLength(0);
+    int animalsPerGroup = groups.GetLength(1);
+
+    for (int i = 0; i < numGroups; i++)
+    {
+        Console.Write($"Group {i+1}: ");
+        for (int j = 0; j < animalsPerGroup; j++)
+        {
+            Console.Write($"{groups[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+    
+    Console.WriteLine();
+}
+
+string[,] AssignGroup(int numGroups = 6)
+{
+    int animalsCount = 0;
+    int animalsPerGroup = pettingZoo.Length/numGroups;
+
+    string[,] groups = new string[numGroups,animalsPerGroup];
+
+    for (int i = 0; i < numGroups; i++)
+    {
+        for (int j = 0; j < animalsPerGroup; j++)
+        {
+            groups[i, j] = pettingZoo[animalsCount];
+            animalsCount++;
+        }
+    }
+
+    return groups;
+}
